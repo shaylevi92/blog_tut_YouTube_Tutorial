f60c90935bc5bbb4fe93060c4b18c2c44e2ecf04
Shay Levi <63535999+shaylevi92@users.noreply.github.com>
2021-11-02 09:09:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-const express = require("express");
-const mongoose = require("mongoose");
+// Import npm packages
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 8080; // Step 1
 
-// ! connect to the mongodb database...
-mongoose.connect(
-  process.env.MONGODB_URI,
-  {
+const routes = require('./routes/api');
+
+// Step 2
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_youtube', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("Connection to mongodb database was successful!");
-  }
-);
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!!');
+});
+
+// Data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Step 3
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
 
 // middlewares
 app.use(express.urlencoded({ extended: true }));
